c381a1d8aeedc480cf442692ff716bb175526082
MukulLambat <mukul04lambat@gmail.com>
2025-04-09 18:09:36 +0200

    initial commit

---
+++ first.py
@@ -0,0 +1,41 @@
+#%%
+print("Hello")
+#%%
+a=int(input("Enter a first number"))
+b=int(input("Enter a second number"))
+#%%
+print("Addition of two numbers are", a+b)
+# %%
+print("Hello","World")
+# %%
+print(type({}))
+# %%
+list1=['a','b','g',1,5]
+print(list1.pop)
+# %%
+var=2
+
+print(2==2.0)
+# %%
+name = 'Hello World!'
+print(name[1:5])
+# %%
+set1={1,5,6,4,3}
+
+print(set1)
+# %%
+a='4.5'
+print(int(a))
+# %%
+a, b = int(input('Enter two Number').split(','))
+# %%
+a = int(input('Enter first Number'))
+b = int(input('Enter second Number'))
+
+c=a+b
+
+print('Sum of %d and %i is %d' %(a,b,c))
+# %%
+a = 15>10
+print(a)
+# %%
---
+++ list.py
@@ -0,0 +1,36 @@
+#%% Create and Print a List
+# Q: Create a list of your 5 favorite fruits and print it.
+
+fruit_list = ['orange', 'apple', 'kiwi', 'pineapple', 'sitafal']
+print(fruit_list)
+# %%
+# ✅ 2. Access Elements by Index
+# Q: Given the list colors = ['red', 'blue', 'green', 'yellow'], print the first and last elements.
+colors = ['red', 'blue', 'green', 'yellow']
+
+print(colors[0],colors[-1])
+
+# %%
+# ✅ 3. Negative Indexing
+# Q: Using the colors list, access and print the second last element using negative indexing.
+colors = ['red', 'blue', 'green', 'yellow']
+print(colors[-2])
+# %%
+# ✅ 4. Slicing a List
+# Q: Print the first 3 elements of numbers = [10, 20, 30, 40, 50] using slicing.
+numbers = [10, 20, 30, 40, 50]
+print(numbers[0:4])
+
+# %%
+# ✅ 5. Modify a List Element
+# Q: Change the third element of the list names = ['Alice', 'Bob', 'Charlie'] to 'David'.
+names = ['Alice', 'Bob', 'Charlie']
+names[2] = 'David'
+print(names)
+# %%
+# ✅ 6. Append an Element
+# Q: Append the number 100 to the list scores = [80, 90, 70].
+scores = [80, 90, 70]
+scores.append(100)
+print(scores)
+# %%
