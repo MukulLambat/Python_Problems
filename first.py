6f40937f0e724e2dd799c67450f2a6ebdabd2c2c
MukulLambat <mukul04lambat@gmail.com>
2025-04-11 09:20:40 +0200

    list

--- first.py
+++ first.py
@@ -58,3 +58,46 @@ if n1 >= n2:
 else:
     print(f"{n2} is greater than {n1}")
 # %%
+# Palindrome Number
+# One way to do it by using string
+num = input("Enter the number to check for Palindrome:")
+num2 = num[-1::-1] #[start:end:step]
+if num == num2:
+    print(f"{num} is a palindrome number")
+else:
+    print(f"{num} not a palindrome number")
+
+# Second way to do it via mathematically
+num = int(input("Enter the number to check for Palindrome:"))
+temp = num
+string_converted = str(num)
+reversed_number = 0
+for i in string_converted:
+    last_digit = temp % 10
+    reversed_number = reversed_number * 10 + last_digit
+    temp = temp // 10
+print(f"{num} is a palindrome number") if num == reversed_number else print(f"{num} not a palindrome number")
+    
+# %%
+# Armstrong Number
+num = int(input("Enter the number to check for Armstrong"))
+temp = num
+string_converted = str(num)
+count = 0
+
+for i in string_converted:
+    count += 1
+raised_addition = 0
+for i in string_converted:
+    last_digit = temp % 10
+    raised_addition += last_digit**count
+    temp = temp // 10
+print("Number is Armstrong") if num == raised_addition else print("Not Armstrong")  
+# %%
+# Nested loop
+n = int(input("Enter the limit"))
+for i in range(n):
+    for j in range(i+1):
+        print(j, end="")
+    print()
+# %%
