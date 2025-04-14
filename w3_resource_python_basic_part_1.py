90ed26df230f04404dd1a5b6cb5233546d00ebf7
MukulLambat <mukul04lambat@gmail.com>
2025-04-14 15:11:54 +0200

    W3_resource basic part1

---
+++ w3_resource_python_basic_part_1.py
@@ -0,0 +1,20 @@
+#%% 1. Formatted Twinkle Poem
+
+# Write a Python program to print the following string in a specific format (see the output).
+# Sample String : "Twinkle, twinkle, little star, How I wonder what you are! Up above the world so high, Like a diamond in the sky. Twinkle, twinkle, little star, How I wonder what you are"
+# Output :
+
+# Twinkle, twinkle, little star,
+# 	How I wonder what you are! 
+# 		Up above the world so high,   		
+# 		Like a diamond in the sky. 
+# Twinkle, twinkle, little star, 
+# 	How I wonder what you are
+
+poem = print("Twinkle, twinkle, little star,\n\
+\tHow I wonder what you are!\n\
+\t\tUp above the world so high,\n\
+\t\tLike a diamond in the sky.\n\
+Twinkle, twinkle, little star,\n\
+\tHow I wonder what you are")
+# %%
